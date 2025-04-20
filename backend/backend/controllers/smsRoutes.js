244439f7134f9b158ed1da5b8256f08a83b6f641
finalprojectdrk <keerudr@gmail.com>
2025-04-20 16:17:01 +0530

    Update and rename smsRoutes.js to smsRoutes

--- backend/backend/controllers/smsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { sendSMS } = require("../controllers/smsController");
-const router = express.Router();
-
-router.post("/sms", sendSMS);
-
-module.exports = router;
