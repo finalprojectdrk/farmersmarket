625eeaf2e0e01ed4ed3a638c7184ab6a0e3e4f7e
finalprojectdrk <keerudr@gmail.com>
2025-04-26 18:21:08 +0530

    Create firebase.js

---
+++ src/utils/firebase.js
@@ -0,0 +1,15 @@
+// src/utils/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+ apiKey: "AIzaSyB6Tw1iQnU5Wy-75wktGgTElp5qLKPOReA",
+  authDomain: "farmersmarket-c89ba.firebaseapp.com",
+  projectId: "farmersmarket-c89ba",
+  storageBucket: "farmersmarket-c89ba.firebasestorage.app",
+  messagingSenderId: "372753875377",
+  appId: "1:372753875377:web:acbcdfb0f13d0776623d90"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
