1a369247b75b58fc2689d55afe7e542cd3f97643
finalprojectdrk <keerudr@gmail.com>
2025-04-26 20:10:02 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -14,7 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
